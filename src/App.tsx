870322e8341947b8bfe7d030cca7f74bd0896a25
Daniel Thomaz <daniel_thls@yahoo.com.br>
2024-08-23 04:55:07 -0300

    Estrutura de rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 
 function App() {
 
   return (
-    <h1>Hello World</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />}>
+          <Route index element={<Home />} />
+          <Route path="products" element={<Products />} >
+            <Route index element={<ProductsHome />} />
+            <Route path="computers" element={<Computers />} />
+            <Route path="electronics" element={<Electronics />} />
+            <Route path="books" element={<Books />} />
+          </Route>
+          <Route path="about" element={<About />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
