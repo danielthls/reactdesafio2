7d5163d09b57cdfd2f2f84f780f11676165601e1
Daniel Thomaz <daniel_thls@yahoo.com.br>
2024-08-24 17:24:43 -0300

    Header home ícone

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,19 @@
 import './styles.css'
+import homeIcon from '../../assets/Vector.png'
 
 export default function Header() {
     return (<header>
         <nav className='de2-container de2-header-nav'>
+
             <ul>
                 <li>Início</li>
                 <li>Produtos</li>
                 <li>Sobre Nós</li>
             </ul>
+
+            <div className='de2-navbar-container'>
+                <img src={homeIcon} alt="Home" />
+            </div>
         </nav>
     </header>
     )
